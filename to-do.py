7a18f1f1eaa90c7a4ba07ba880b4fdf341bb749a
Kartik Neti <kartikprajak@gmail.com>
2023-10-21 14:39:28 +0530

    Initial commit

--- to-do.py
+++ to-do.py
@@ -6,7 +6,7 @@ todos = function.get_todos()
 st.title("My  Todo")
 st.subheader("This is my to-do app")
 st.write("To increase productivity")
-# st.header('This is a header', divider= 'grey')
+st.header('This is a header', divider= 'grey')
 
 for todo in todos:
     st.checkbox(todo)
