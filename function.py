419df12ae9370f42603f05cbc3363f29ac2d03d6
Kartik Neti <kartikprajak@gmail.com>
2023-10-21 16:13:35 +0530

    Todo MVP

--- function.py
+++ function.py
@@ -14,7 +14,18 @@ def write_todos(todos_arg, filepath=FILEPATH):
     """ Write the to-do items list in the text file."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
+        
+      
+def complete_todos(dones):
+    with open('dones.txt', 'a+') as done:
+        done.writelines(dones)
 
+def get_done_todos():
+    with open('dones.txt', 'r') as file_local:
+        
+        done_todos_local = file_local.readlines()
+        # top5 = done_todos_local[:5]
+    return done_todos_local
 
 if __name__ == "__main__":
     print("Hello")
--- to-do.py
+++ to-do.py
@@ -2,6 +2,8 @@ import streamlit as st
 import function 
 
 todos = function.get_todos()
+dones = function.get_done_todos()
+done_todo = ''
 
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
@@ -9,12 +11,27 @@ def add_todo():
     function.write_todos(todos)
 
 
-st.title("My  Todo")
-st.subheader("This is my to-do app")
-st.write("To increase productivity")
-st.header('This is a header', divider= 'grey')
+st.title("My Todo")
+# st.subheader("This is my to-do app")
+# st.write("To increase productivity")
+st.header('Add a Todo', divider= 'grey')
+st.text_input(label="Todo input box", label_visibility= 'hidden', placeholder='Add a new todo..', on_change= add_todo, key='new_todo')
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        function.complete_todos(dones=todo)
+        todos.pop(index)
+        function.write_todos(todos)
+        # del st.session_state[todo]
+        st.rerun()
+        
+st.header('Last 5 completed todos', divider= 'grey')
+top5 = dones[-1:-6:-1]
+for i in top5:
+    st.write(i)
     
-st.text_input(label="Todo input box",label_visibility= 'hidden', placeholder='Add a new todo..', on_change= add_todo, key='new_todo')
+with st.expander("Click here to show all todos"):
+    for done in dones:
+        st.write(done)
+    
