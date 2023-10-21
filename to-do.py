c329bf646d1de02646fcc5391bff728c7cde9458
Kartik Neti <kartikprajak@gmail.com>
2023-10-21 14:50:58 +0530

    Add new items feature added

--- to-do.py
+++ to-do.py
@@ -3,6 +3,12 @@ import function
 
 todos = function.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo) 
+    function.write_todos(todos)
+
+
 st.title("My  Todo")
 st.subheader("This is my to-do app")
 st.write("To increase productivity")
@@ -11,4 +17,4 @@ st.header('This is a header', divider= 'grey')
 for todo in todos:
     st.checkbox(todo)
     
-st.text_input(label="",placeholder='Add a new todo..')
+st.text_input(label="Todo input box",label_visibility= 'hidden', placeholder='Add a new todo..', on_change= add_todo, key='new_todo')
